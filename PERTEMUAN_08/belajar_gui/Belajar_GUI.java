8c2ac0c62d7a062f65037146a4477b6ed20f4ba7
NICBot <129926368+ncbot2022@users.noreply.github.com>
2025-12-03 12:05:26 +0700

    Add files via upload

---
+++ PERTEMUAN_08/belajar_gui/Belajar_GUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PERTEMUAN_8.belajar_gui;
+
+/**
+ *
+ * @author x260
+ */
+public class Belajar_GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello Word");
+    }
+    
+}
